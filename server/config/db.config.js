8e5e67b7b5892b87a5169413cb7571cb35f31639
Abdallah Altokhy <abdallahaltokhy@gmail.com>
2021-03-05 15:33:45 +0100

    Connected to stripe & payment form works

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,5 +3,6 @@ module.exports = {
   USER: "root",
   PASSWORD: "test123456",
   DB: "hogwarts",
-  secretToken: "MYSECRETTOKENFORJWT"
+  secretToken: "MYSECRETTOKENFORJWT",
+  stripeSecret: "sk_test_51IQB1zJgjiR5JIS9RLcCwmbQ5RjgZWq3r3RObRRvJ8onoKT0A8nPEtIB0Gul3U29YGoPaWFWlwAC1OYZnFYl3bB100dKRL9tqi"
 };
