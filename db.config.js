259b217d64f2fb1b164c9ddbb7c2071e46dcd45d
huss <hussein.odeh@gmail.com>
2021-03-11 15:09:12 +0100

    fix: package.json

---
+++ db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  HOST: 'localhost',
+  USER: 'hussein',
+  PASSWORD: 'nopukmas',
+  DB: 'hogwarts',
+  secretToken: 'MYSECRETTOKENFORJWT',
+  stripeSecret:
+    'sk_test_51IQB1zJgjiR5JIS9RLcCwmbQ5RjgZWq3r3RObRRvJ8onoKT0A8nPEtIB0Gul3U29YGoPaWFWlwAC1OYZnFYl3bB100dKRL9tqi',
+};
