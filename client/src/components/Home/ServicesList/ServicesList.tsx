d6a9860a977d012beb0d3855ab69e9c493bfb569
cxgarcia <schlaubitzcristobal@gmail.com>
2021-03-13 15:26:02 +0100

    REFAC: home components to ts

--- client/src/components/Home/ServicesList/ServicesList.tsx
+++ client/src/components/Home/ServicesList/ServicesList.tsx
@@ -4,40 +4,36 @@ import { Link } from '@reach/router';
 import './ServicesList.css';
 
 const services = [
-	'Home Cleaning',
-	'Plumbing',
-	'Carpentry',
-	'Car Services',
-	'Electricity',
-	'Painting',
-	'Furniture assembly',
-	'Appliance installation',
-	'Disinfection & Sterilization',
-	'Tile & Ceramic Installation',
+  'Home Cleaning',
+  'Plumbing',
+  'Carpentry',
+  'Car Services',
+  'Electricity',
+  'Painting',
+  'Furniture assembly',
+  'Appliance installation',
+  'Disinfection & Sterilization',
+  'Tile & Ceramic Installation'
 ];
 
-const ServicesList = () => {
-	return (
-		<div id="SERVICES">
-			<h1 className="Services-Title">Our Services</h1>
-			<div className="service-list">
-				{services.map((service) => {
-					return (
-						<div key={service} className="service-box">
-							<Link className="service-box-inner" to="/checkout">
-								<img
-									className="services-img"
-									src={'/imgs/' + service + '.png'}
-									alt={service}
-								></img>
-								<h3 className="service-head">{service}</h3>
-							</Link>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+const ServicesList: React.FC = () => {
+  return (
+    <div id="SERVICES">
+      <h1 className="Services-Title">Our Services</h1>
+      <div className="service-list">
+        {services.map((service) => {
+          return (
+            <div key={service} className="service-box">
+              <Link className="service-box-inner" to="/checkout">
+                <img className="services-img" src={'/imgs/' + service + '.png'} alt={service}></img>
+                <h3 className="service-head">{service}</h3>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ServicesList;
