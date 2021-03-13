6449993e59117ca479b1e1f64d9a3cf10c3ac06c
Cristobal Schlaubitz Garcia <57994942+CxGarcia@users.noreply.github.com>
2021-03-13 11:28:39 +0100

    Update config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,7 +3,6 @@ const isTest = process.env.NODE_ENV === 'test';
 module.exports = {
   development: {
     database: process.env[`DB_NAME${isTest ? '_TEST' : ''}`],
-    database: isTest ? process.env.DB_NAME_TEST : process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
